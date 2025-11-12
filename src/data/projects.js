db643dc69af4c4ba9f375f29ceb09cfdeb24e092
benchav <joshua44benja@gmail.com>
2025-11-12 16:31:41 -0600

    news

--- src/data/projects.js
+++ src/data/projects.js
@@ -48,57 +48,64 @@ const projects = [
       image: "https://i.ibb.co/Q7HmPjzj/cutler.png",
       link: "https://cutlergym.vercel.app/"
     },
-        {
+    {
       id: 8,
       title: "Portafolio Web",
       description: "Portafolio de Diseño grafico.",
       image: "https://i.ibb.co/MybkdGj5/portafoliomario.png",
       link: "https://portafoliomariosamuel.vercel.app/"
     },
-      {
+    {
       id: 9,
       title: "Fast-api",
       description: "Fast api en python para enviar correos con resend.",
       image: "https://i.ibb.co/KcqPG7nd/fastapi.png",
       link: "https://emailapijb.vercel.app/"
     },
-    {
+     {
       id: 10,
+      title: "Edu-Incluye",
+      description: "Aplicación web para estudio y aprendisaje del lenguaje de señas Nicaraguense",
+      image: "https://i.ibb.co/8LTqs8x5/Edu-Incluye.jpg",
+      link: "https://edu-incluye.vercel.app/"
+    },
+    {
+      id: 11,
       title: "Tasko",
       description: "Aplicación de productividad, gestiòn y organizaciòn de tareas usando React native js y fastapi python",
       image: "https://i.ibb.co/FLncfXVR/Home.jpg",
       link: "https://github.com/Benchav"
     },
     {
-      id: 10,
+      id: 12,
       title: "InsBarrera",
       description: "Apliación de compra de productos pasteles, carrito de compras, compra, inventario usando Flutter y Api en C#",
       image: "https://i.ibb.co/TDq2QfjQ/insbarrera.jpg",
       link: "https://github.com/Benchav/APP_InsBarrera.git"
     },
-        {
-      id: 11,
+    {
+      id: 13,
       title: "fast tasko",
       description: "fast api de la apliación tasko, usando python, crud completo y autenticación base de datos en firebase",
       image: "https://i.ibb.co/zhNhsnHP/fasttasko.jpg",
       link: "https://fasfttasko.vercel.app/docs#/"
     },
     {
-      id: 12,
+      id: 14,
       title: "Api insumos barrera",
       description: "Api en ASP.Net usando C#, crud completo",
       image: "https://i.ibb.co/hJnnY9GL/api-insumosbarrera.jpg",
       link: "https://github.com/Benchav/APIRestInsumos.git"
     },
     {
-      id: 13,
+      id: 15,
       title: "Api zapatos",
       description: "Api completa para gestión de zapatos por roles y permisos, usando Node.js y JavaScript",
       image: "https://i.ibb.co/Pkr2dX1/payless-api.jpg",
       link: "https://github.com/Benchav/Payless-Api.git"
     },
     {
-      id: 14,
+      id: 16,
       title: "Api Constructora",
       description: "Api diseñada en node.js en JavaScript, JWT token, roles y permisos, 13 modelos y su crud completo, protección de rutas y validaciones",
       image: "https://i.ibb.co/sJVvM3NM/api-constructora.jpg",
